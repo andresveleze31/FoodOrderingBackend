fcf0bf21a7772f0f132dd09ae155571877bde8a9
Andres Velez <andresvelezecheverry@gmail.com>
2024-06-27 11:22:23 -0500

    Get Single Restaurant

--- src/routes/FoodRestaurantRoutes.ts
+++ src/routes/FoodRestaurantRoutes.ts
@@ -5,14 +5,21 @@ import FoodRestaurantController from "../controller/FoodRestaurantController";
 const router = express.Router();
 
 router.get(
+  "/:restaurantId",
+  param("restaurantId")
+    .isString()
+    .trim()
+    .withMessage("RestaurantId parameter must be valid string"),
+    FoodRestaurantController.getRestaurant
+);
+
+router.get(
   "/search/:city",
   param("city")
     .isString()
     .trim()
     .withMessage("City parameter must be valid string"),
-    FoodRestaurantController.searchRestaurants
+  FoodRestaurantController.searchRestaurants
 );
 
 export default router;
-
-
